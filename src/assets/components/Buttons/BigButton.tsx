819488413c13fb631031c5a7ccca3125a7faf6cb
Patrick <accounts@pp83.de>
2025-08-26 19:24:15 +0200

    feat: add BigButton component for enhanced button functionality

---
+++ src/assets/components/Buttons/BigButton.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@mui/material";
+
+type BigButtonProps = {
+	label: string;
+} & React.ComponentProps<typeof Button>;
+
+const BigButton = ({ label, sx, ...props }: BigButtonProps) => {
+	return (
+		<Button
+			variant="contained"
+			color="primary"
+			onClick={props.onClick}
+			sx={{
+				fontSize: "1.25rem",
+				padding: "0.75rem 1.5rem",
+				minWidth: "150px",
+				...sx,
+			}}
+			{...props}
+		>
+			{label}
+		</Button>
+	);
+};
+
+export default BigButton;
